8ea17734de440ff3db14c708d58b32beb812072d
Antr4ss <harold.guerrero02@uptc.edu.co>
2024-04-30 22:45:04 -0500

    Funcion 1 terminada

--- main.c
+++ main.c
@@ -2,25 +2,7 @@
 
 #include <string.h>
 
-int buscarSubcadena(char *string, char *substring, int initialPosition) {
-    int i, j;
-
-    for (i = initialPosition; i < strlen(string); i++) {
-        if (string[i] == substring[0]) {
-            for (j = 1; j < strlen(substring); j++) {
-                if (string[i + j] != substring[j]) {
-                    break;
-                }
-            }
-
-            if (j == strlen(substring)) {
-                return i;
-            }
-        }
-    }
-
-    return -1;
-}
+int buscarSubcadena(char *string, char *substring, int initialPosition);
 
 int main(void) {
     int option;
@@ -94,6 +76,26 @@ int main(void) {
         }
     } while(option!=0);
     return 0;
+}
+
+int buscarSubcadena(char *string, char *substring, int initialPosition) {
+    int i, j;
+
+    for (i = initialPosition; i < strlen(string); i++) {
+        if (string[i] == substring[0]) {
+            for (j = 1; j < strlen(substring); j++) {
+                if (string[i + j] != substring[j]) {
+                    break;
+                }
+            }
+
+            if (j == strlen(substring)) {
+                return i;
+            }
+        }
+    }
+
+    return -1;
 }
 
 
