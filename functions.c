fa9798d4f48267c39ec7a93c498f48784edc4f7e
Estevan Vivas <estevanvivas604@gmail.com>
2024-05-01 21:05:04 -0500

    insert_substr function implemented

--- functions.c
+++ functions.c
@@ -43,3 +43,35 @@ char *str_diff(const char *first_str, const char *second_str) {
 
     return result;
 }
+
+
+char *insert_substr(const char *base_str, const char *substr, int position) {
+    int base_len = string_length(base_str);
+    int substr_len = string_length(substr);
+
+    if (position < 0 || position > base_len) {
+        return NULL;
+    }
+
+    int total_len = base_len + substr_len + 1;
+
+    char *result = malloc(sizeof(char) * total_len);
+    if (!result) {
+        return NULL;
+    }
+
+    for (int i = 0; i < position; ++i) {
+        result[i] = base_str[i];
+    }
+
+    for (int i = 0; i < substr_len; ++i) {
+        result[position + i] = substr[i];
+    }
+
+    for (int i = position; i < total_len; ++i) {
+        result[substr_len + i] = base_str[i];
+    }
+
+    result[total_len - 1] = '\0';
+    return result;
+}
--- functions.h
+++ functions.h
@@ -7,4 +7,6 @@ int character_exists(const char *source, char target);
 
 char *str_diff(const char *first_str, const char *second_str);
 
+char *insert_substr(const char *base_str, const char *substr, int position);
+
 #endif //FUNCTIONS_H
